73c1d9d8c56875ee33fd622253cb9c0ad6f53c47
kygm02 <kygm02@gmail.com>
2026-05-10 15:07:38 +0100

    set BR2_DL_DIR from defconfig insted of build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 source shared.sh
 export HOSTCC=$(command -v gcc-13 || command -v gcc)
 export HOSTCXX=$(command -v g++-13 || command -v g++)
-export BR2_DL_DIR=${HOME}/.dl
+#export BR2_DL_DIR=${HOME}/.dl
 
 
 EXTERNAL_REL_BUILDROOT=../base_external
@@ -23,18 +23,15 @@ then
 	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
 	then
 		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
-		sudo chown -R $(whoami):$(whoami) .
 		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT} -j 16
 	else
 		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
 		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
-		sudo chown -R $(whoami):$(whoami) .
 		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG} -j 16
 	fi
 else
 	echo "USING EXISTING BUILDROOT CONFIG"
 	echo "To force update, delete .config or make changes using make menuconfig and build again."
-	sudo chown -R $(whoami):$(whoami) .
 	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} -j 16
 
 fi
